ce0c1ccfa693657d504fd79ec69ff425b4030e36
Kili03 <Kilian.bechtold@mail.com>
2024-12-04 00:09:15 +0100

    Reading Data into Vector

--- Day2/test.cpp
+++ Day2/test.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    cout << "Hello World" << endl;
+
+    ifstream file("data.txt");
+
+    vector<vector<int>> lines;
+    string line;
+    while (getline(file, line)) {
+        // cout << line << endl;
+        stringstream lineStream(line);
+
+        vector<int> numbers;
+        int number;
+        while (lineStream >> number) {
+            numbers.push_back(number);
+            // cout << number << " ";
+        }
+        lines.push_back(numbers);
+        // cout << endl;
+
+    }
+
+    for (vector<int> int_line : lines) {
+        for (int num : int_line) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    cout << "Hi";
+
+    return 0;
 }
