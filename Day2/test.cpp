d24686e1829b53ba0a7cd60cff4182c7c9759d0e
Bechtold, Kilian Johannes <kbechtol@students.uni-mainz.de>
2024-12-04 20:42:20 +0100

    Continued Day 2.1

--- Day2/test.cpp
+++ Day2/test.cpp
@@ -4,6 +4,29 @@
 #include <vector>
 using namespace std;
 
+bool check(vector<int> line) {
+    if (line.size() == 1) {
+        return true;
+    }
+    bool increasing;
+    int first = line.at(0);
+    int second = line.at(1);
+    increasing = first < second;
+
+    // cout << "size: " << line.size() << " elements: ";
+    for (int i = 0; i < line.size() - 1; i++) {
+        cout << line[i+1] << " ";
+        if (line[i] == line[i + 1]) {
+            return false;
+        }
+        if ((line[i] < line[i + 1]) != increasing) {
+            return false;
+        }
+    }
+    cout << endl;
+    return true;
+}
+
 int main()
 {
     cout << "Hello World" << endl;
@@ -12,6 +35,9 @@ int main()
 
     vector<vector<int>> lines;
     string line;
+    int count = 0;
+    int all = 0;
+
     while (getline(file, line)) {
         // cout << line << endl;
         stringstream lineStream(line);
@@ -22,18 +48,27 @@ int main()
             numbers.push_back(number);
             // cout << number << " ";
         }
+        if (check(numbers)) {
+            count++;
+        }
+        all++;
         lines.push_back(numbers);
         // cout << endl;
 
     }
 
-    for (vector<int> int_line : lines) {
+    /*for (vector<int> int_line : lines) {
         for (int num : int_line) {
             cout << num << " ";
         }
         cout << endl;
-    }
-    cout << "Hi";
+    }*/
+    cout << "Hi" << endl;
+    cout << count << endl;
+    cout << all << endl;
+
+    /*int hallotest;
+    cout << hallotest;*/
 
     return 0;
 }
