c01c2a35960ff415f1c72967750fcf5623e448d1
Kili03 <Kilian.bechtold@mail.com>
2025-03-30 19:30:34 +0200

    Day 6 part two test 2

--- Day6/part_two.cpp
+++ Day6/part_two.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <queue>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -16,10 +17,6 @@ bool contains(const vector<T>& vec, const T& element) {
 struct Vec2d {
     int d_row, d_column;
 
-    Vec2d operator*(const int multiplicand) const {
-        return {d_row * multiplicand, d_column * multiplicand};
-    }
-
     bool operator==(const Vec2d &other) const {
         return d_row == other.d_row && d_column == other.d_column;
     }
@@ -29,7 +26,7 @@ struct Position {
     int row, column;
 
     [[nodiscard]] bool is_on_edge(const vector<vector<char>>& grid) const {
-        return row == 0 || column == 0 || row == grid.size() - 1 || column == grid[0].size() - 1;
+        return row <= 0 || column <= 0 || row >= grid.size() - 1 || column >= grid[0].size() - 1;
     }
 
     Position operator+(const Vec2d& vec) const {
@@ -45,7 +42,7 @@ struct Position {
     }
 
     [[nodiscard]] string toString() const {
-        return to_string(row) + to_string(column);
+        return to_string(row) + " " + to_string(column);
     }
 };
 
@@ -55,35 +52,13 @@ struct PositionHash {
     }
 };
 
-struct Line {
-    Position end_pos;
-    Vec2d direction;
-    int length;
-
-    Line(const Position end, const Vec2d dir, const int len)
-        : end_pos(end), direction(dir), length(len) {}
-
-    [[nodiscard]] bool check_possible(const Position& pos) const {
-        if (direction.d_row == 0) {
-            return end_pos.row == pos.row;
-        }
-        if (direction.d_column == 0) {
-            return end_pos.column == pos.column;
-        }
-        throw invalid_argument("Invalid distance vector");
-    }
-};
-
 struct Grid {
     vector<vector<char>> grid;
     Position current_position;
-    unordered_map<Position, vector<Vec2d>, PositionHash> direction_dict;
-    int visited_cells = 1;
-    vector<Line> lines;
-    int moved_without_turn = 0;
+    unordered_map<Position, vector<Vec2d>, PositionHash> direction_dict{};
     int direction_index = 0;
     constexpr static Vec2d directions[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    int loop_count = 0;
+    unordered_set<Position, PositionHash> loop_obstacles;
 
     Grid(const vector<vector<char>>& grid_vec, const Position& start_pos)
         : grid(grid_vec), current_position(start_pos) {
@@ -97,73 +72,53 @@ struct Grid {
         grid[pos.row][pos.column] = new_char;
     }
 
-    [[nodiscard]] bool find_loop_with_line(const Line &line) const {
-        if (line.check_possible(current_position)) {
-            const Vec2d next_direction = directions[(direction_index+1)%4];
-            Position look_position = current_position;
-            while (!look_position.is_on_edge(grid) && get_char(look_position) != '#' && look_position != line.end_pos) {
-                look_position = look_position + next_direction;
-            }
-            if (look_position == line.end_pos) {
+    bool find_loop() const {
+        unordered_map<Position, vector<Vec2d>, PositionHash> direction_test_dict;
+        direction_test_dict[current_position].emplace_back(directions[direction_index]);
+
+        if (get_char(current_position + directions[direction_index]) == '^') {
+            return false;
+        }
+
+        Position next_test_pos = current_position;
+        int direction_test_index = (direction_index + 1) % 4;
+
+        while (!next_test_pos.is_on_edge(grid)) {
+            if (contains(direction_test_dict[next_test_pos], directions[direction_test_index])) {
+                // cout << "Found loop after " << checked_positions << " positions" << endl;
                 return true;
             }
+            direction_test_dict[next_test_pos].emplace_back(directions[direction_test_index]);
+
+            if (get_char(next_test_pos + directions[direction_test_index]) == '#') {
+                direction_test_index = (direction_test_index + 1) % 4;
+            } else {
+                next_test_pos = next_test_pos + directions[direction_test_index];
+            }
+
         }
         return false;
     }
 
-    bool find_loop() {
-        if (contains(direction_dict[current_position], directions[direction_index])) {
-            return true;
-        }
-        Position next_test_pos = current_position;
-        int direction_test_index = direction_index;
-        while (true) {
-            next_test_pos = next_test_pos + directions[direction_index];
-            if (next_test_pos.is_on_edge(grid)) {
-                return false;
-            }
-            if (get_char(next_test_pos + directions[direction_test_index]) == '#') {
-                direction_test_index = (direction_test_index + 1) % 4;
-            }
-            if (contains(direction_dict[next_test_pos], directions[direction_test_index])) {
-                return true;
-            }
-        }
-    }
-
-    bool move() {
-        Vec2d direction = directions[direction_index];
-        const Position next_pos = current_position + direction;
+    void move() {
+        Position next_pos = current_position + directions[direction_index];
         if (get_char(next_pos) == '#') {
-            lines.emplace_back(current_position, direction, moved_without_turn);
-            moved_without_turn = 0;
             direction_index = (direction_index + 1) % 4;
-            return false;
+            // next_pos = current_position + directions[direction_index];
+        } else {
+            current_position = next_pos;
         }
-        if (get_char(next_pos) == '.') {
-            set_char(next_pos, 'X');
-            visited_cells++;
-        }
-        direction_dict[current_position].emplace_back(directions[direction_index]);
-        current_position = next_pos;
-        moved_without_turn++;
 
-        /*for (int i=static_cast<int>(lines.size())-3; i>=0; i-=4) {
-            if (find_loop_with_line(lines[i])) {
-                loop_count++;
-                break;
-            }
-        }*/
+        if (current_position.is_on_edge(grid)) {
+            return;
+        }
+        // direction_dict[current_position].emplace_back(directions[direction_index]);
 
         if (find_loop()) {
-            loop_count++;
+            if (!loop_obstacles.contains(current_position + directions[direction_index])) {
+                loop_obstacles.emplace(current_position + directions[direction_index]);
+            }
         }
-
-        return true;
-    }
-
-    [[nodiscard]] bool is_on_edge() const {
-        return current_position.is_on_edge(grid);
     }
 };
 
@@ -176,15 +131,14 @@ Grid build_grid(const string& path) {
     vector<vector<char>> grid;
     grid.reserve(130);
     string line;
-    Position final_pos{};
+    Position final_start_pos{};
     int row = 0;
     while (getline(File, line)) {
         grid.emplace_back(line.begin(), line.end());
         int column = 0;
-        for (char c: line) {
+        for (const char c: line) {
             if (c == '^') {
-                final_pos = Position(row, column);
-                grid[row][column] = '.';
+                final_start_pos = Position(row, column);
             }
             column++;
         }
@@ -192,18 +146,18 @@ Grid build_grid(const string& path) {
     }
 
     // cout << "Start Pos: " << final_pos.row << "," << final_pos.column << endl;
-    return {grid, final_pos};
+    return {grid, final_start_pos};
 }
 
 
 int code() {
     Grid grid = build_grid("Advent-of-Code/Day6/data.txt");
 
-    while (!grid.is_on_edge()) {
+    while (!grid.current_position.is_on_edge(grid.grid)) {
         grid.move();
     }
 
-    return grid.loop_count;
+    return grid.loop_obstacles.size();
 }
 
 
